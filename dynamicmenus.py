27810338850d8b698a3a4a7275b632156a2bce93
Rithvik Kandula <rkandula8284@hwdsb.on.ca>
2025-10-06 18:11:54 -0400

    first commit

---
+++ dynamicmenus.py
@@ -0,0 +1,48 @@
+###############################################################################
+#  Program Name   : dynamicmenus.py
+#  Author         : Rithvik Kandula
+#  Task           : Create a Python program that allows a user to create their own list 
+#                   (of names, fruits, countries, etc.), view that list as a numbered menu, and then interact with the list by:
+#                   Selecting an item from the menu to display.
+#                   Adding a new item to the list.
+#                   Exiting the program.
+###############################################################################
+
+#Asking for the inital items in the list
+mydic = {input("Enter the index for the first item: "): input("Enter the first item: "), 
+        input("Enter the index for the second item: "): input("Enter the second item: "), 
+        input("Enter the index for the third item: "): input("Enter the third item: "), 
+        input("Enter the index for the fourth item: "): input("Enter the fourth item: ")}
+
+print("\n", "-"*30, "\n")
+while True:
+    #printing the list
+    for i in mydic:
+        print(i, ":", mydic[i])
+    
+    choice = input("If you want to add an item, type 'A'\nIf you want to access an item from your dictionary, type the correspondign index \nIf you want to end the program, type '0'\n -> ")
+    if choice == "A": #If users selects A to add a new index and item
+        index = "A"
+        item = "A"
+
+        while index == "A" or index == "0": #Making sure the index is not A or 0
+            index = input("Enter the new index(cannot be A or 0): ")
+        
+        item = input("Enter the new item: ")
+        mydic[index] = item
+        print("\nItem added!\n", "-"*30)
+
+    elif choice == "0": #If user selects 0 to end the program
+        print("\n", "-"*30, "\nThank you, Goodbye!")
+        break
+
+    else: #If user wants to access an item
+        inlist = False
+        for n in mydic: #checking if the item that the user wants to access is in the list
+            if n == choice:
+                print("\n", "-"*30, "\nYour item is:", mydic[choice])
+                inlist = True
+        if not inlist: #If item is not in the list
+            print("\n", "-"*30,"\nItem not in list")
+
+    input("Continue:\n" + "-"*30)

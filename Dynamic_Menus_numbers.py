8ba09b742ea7faaad3769feacdb9be5e57ba6915
Rithvik Kandula <rkandula8284@hwdsb.on.ca>
2025-10-17 08:57:35 -0400

    sync

--- Dynamic_Menus_numbers.py
+++ Dynamic_Menus_numbers.py
@@ -7,6 +7,13 @@
 #                   Adding a new item to the list.
 #                   Exiting the program.
 ###############################################################################
+def is_num(s):
+      try:
+        int(s)  # or float(s) for decimals
+        return True
+      except ValueError:
+        return False
+
 valid_input = False
 while not valid_input:
     items = input("Enter indexes and items in the following format:\n'index 1': 'number 1', 'index 2': 'number 2', etc\n-> ")
@@ -20,8 +27,11 @@ while not valid_input:
     mydic = {}
     for i in iandi: #seperating between semicolons
         mydic[i.split(":")[0].strip()] = i.split(":")[1].strip()
-        if not (i.split(":")[1].strip()).isnumeric():
+        if not is_num(i.split(":")[1].strip()):
             print("Your item nubmer is not a number!\n" + "-"*30 + "\n")
+            valid_input = False
+        if i.split(":")[0].strip() == 'A' or i.split(":")[0].strip() == '0' or i.split(":")[0].strip() == 'N':
+            print("Your index cannot be A, 0, or N!\n" + "-"*30 + "\n")
             valid_input = False
 
 
@@ -42,16 +52,24 @@ while True:
         print(i, ":", mydic[i])
         added_item += i + " : "+ mydic[i]+ "\n"
     
-    choice = input("If you want to add an item, type 'N'\nIf you want to access an item from your dictionary, type the correspondign index \nIf you want to end the program, type '0'\n -> ")
+    choice = input("If you want to add an item, type 'N'\nIf you want to increase ro decrease one of the amounts, type 'A'\nIf you want to access an item from your dictionary, type the correspondign index \nIf you want to end the program, type '0'\n -> ")
     if choice == "N": #If users selects N to add a new index and item
         index = "N"
-
         while index == "N" or index == "0": #Making sure the index is not N or 0
-            index = input("Enter the new index(cannot be N or 0): ").strip()
+            index = input("Enter the new index(cannot be N, A, 0): ").strip()
         
-        item = input("Enter the new item: ")
-        mydic[index] = item.strip()
+        item = "h"
+        while not is_num(item):
+            item = input("Enter the new number: ")
+            mydic[index] = item.strip()
         print("\nItem added!\n", "-"*30)
+
+    elif choice == "A":
+        index_chan = input("What is the index you want to change?: ")
+        num_chan = "h"
+        while not is_num(num_chan):
+            num_chan = input("How much do you want to add (5) or subtract (-5)")
+        mydic[index_chan] = str(int(mydic[index_chan]) + int(num_chan))
 
     elif choice == "0": #If user selects 0 to end the program
         print("\n", "-"*30, "\nThank you, Goodbye!")
